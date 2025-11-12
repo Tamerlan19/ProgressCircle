f2a01398b024849d696a9041ddadc66b73e957bb
Tamerlan19 <tamerlansaraliev@gmail.com>
2025-11-12 17:23:43 +0300

    addSecondCircle

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,8 @@ const hideToggle = document.getElementById('hide-toggle');
 const progressCircle = document.querySelector('.progress__circle');
 
 const arc = document.querySelector('.progress__arc');
+const knobRot = document.querySelector('.knob-rot'); 
 const input = document.getElementById('value-input');
-
 const animateToggle = document.getElementById('animate-toggle');
 
 
@@ -13,16 +13,51 @@ hideToggle.addEventListener('change', () => {
 
 
 animateToggle.addEventListener('change', () => {
-  if (animateToggle.checked) {
+
+  function loop() {
+    const raw = input.value.trim();
+    const correctValue = raw !== '' && !isNaN(raw);
+    const value = Math.min(Math.max(parseInt(raw, 10) || 0, 0), 100);
+
+    if (!(animateToggle.checked && correctValue)) return;
+
+    knobRot.style.transition = 'none';
+    rotateKnobToValue(0);
+
+    setTimeout(() => {
+      knobRot.style.transition = 'transform 1.0s linear';
+      rotateKnobToValue(value);
+    }, 50);
+
+    setTimeout(loop, 1100);
+  }
+
+  const strokaInput = input.value.trim();
+  const correctValue = strokaInput !== '' && !isNaN(strokaInput);
+  if (animateToggle.checked && correctValue) {
     arc.style.transition = 'stroke-dashoffset 0.6s ease-in-out';
+    knobRot.style.opacity = '1';
+    loop();
   } else {
     arc.style.transition = 'none';
+    knobRot.style.opacity = '0';
+    rotateKnobToValue(0);
   }
 });
 
 
+knobRot.style.animationPlayState = 'paused';
+knobRot.style.opacity = '0';
 
 const CIRCLE_LENGTH = 2 * Math.PI * 60;
+
+function rotateKnobToValue(value) {
+const degrees = value === 0 ? 0 : 360 * (value / 100) - 18;
+
+  knobRot.style.transform = `rotate(${degrees}deg)`;
+}
+
+
 
 input.addEventListener('input', () => {
   const raw = input.value.trim();
@@ -31,6 +66,7 @@ input.addEventListener('input', () => {
     input.title = 'Введите число от 0 до 100';
     input.classList.remove('invalid');
     arc.style.strokeDashoffset = CIRCLE_LENGTH;
+    arc.style.strokeDasharray = CIRCLE_LENGTH; 
     return;
   }
 
@@ -49,6 +85,8 @@ input.addEventListener('input', () => {
 
   input.classList.remove('invalid');
 
+  arc.style.strokeDasharray = CIRCLE_LENGTH;
   const offset = CIRCLE_LENGTH * (1 - value / 100);
   arc.style.strokeDashoffset = offset;
+  rotateKnobToValue(value);
 });
